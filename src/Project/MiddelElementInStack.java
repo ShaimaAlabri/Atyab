10f182379121a29fe4b055ab6dafc4e4638765d3
unknown <shaima96alabri@hotmail.com>
2023-01-16 23:12:31 -0800

    middel element other way

--- src/Project/MiddelElementInStack.java
+++ src/Project/MiddelElementInStack.java
@@ -13,7 +13,15 @@ names.push("Ahmed");
 names.push("Tamim");
 names.push("Aish");
 System.out.println("the middel elimeint is :"+ names.get(2));
-
+// other solution
+Stack<String> color=new Stack<>();
+color.push("Red");
+color.push("Blue");
+color.push("Pink");
+color.push("Black");
+color.push("Green");
+String middle = color.get(color.size()/2);
+System.out.println("Middle Element: " + middle);
 	}
-
+	
 }
