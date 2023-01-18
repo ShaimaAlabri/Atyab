c2775efc899a51c8f9570c68234e3a9588c0740b
unknown <shaima96alabri@hotmail.com>
2023-01-17 21:58:43 -0800

    next greater element

---
+++ src/Project/NextGreaterElement.java
@@ -0,0 +1,15 @@
+package Project;
+
+import java.util.Arrays;
+
+public class NextGreaterElement {
+	public static void main(String[] args) {
+//		 For the sixth problem, you would need to provide an array of integers as input to the function that finds the next greater element.
+		int[] num= {1,5,6,9,7,10};
+		int size=num.length;
+		Arrays.sort(num);
+		   System.out.println("sorted Array ::"+Arrays.toString(num));
+		      int res = num[size-2];
+		      System.out.println("2nd largest element is ::"+res);
+	}
+}
--- src/Project/PostFixiedExprissoin.java
+++ src/Project/PostFixiedExprissoin.java
@@ -9,8 +9,7 @@ public class PostFixiedExprissoin {
 
 	public static void main(String[] args) {
 		//Create a function in Java that evaluates a postfix expression using a stack.
-//	static int evalPf(String str)
-//	{
+
 		Stack <String> post= new Stack<>();
 		post.push("-");
 		post.push("+");
